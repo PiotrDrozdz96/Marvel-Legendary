7bf9c7ab25051eb16c68eeca99ec58aab63354a3
Piotr Drożdż <piotr.drozdz0@op.pl>
2018-07-12 15:00:53 +0200

    prepare decks in boardService

--- src/app/board.service.ts
+++ src/app/board.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
+
 import { Deck } from './models/deck';
-import { Hero, Scheme, Mastermind, Bystander } from './models/card';
+import { Hero, Scheme, Mastermind, Bystander, CardInVillainDeck, Villain } from './models/card';
+
 import { hero_shield_agent, hero_shield_trooper, hero_shield_officer } from './cards/hero/shield';
 import { scheme_legacy_virus } from './cards/scheme';
 import { mastermind_magneto } from './cards/mastermind';
@@ -14,20 +16,42 @@ import { bystander } from './cards/bystanders';
 export class BoardService {
 
   private koImage = new BehaviorSubject<string>('');
+
   private playerDeck = new Deck<Hero>();
-  scheme: Scheme = new scheme_legacy_virus;
-  mastermind: Mastermind = new mastermind_magneto;
+  private playerHand = new Deck<Hero>();
+  private discardPile = new Deck<Hero>();
+
   shieldDeck = new Deck<hero_shield_officer>();
   woundsDeck = new Deck<Hero>();
   bystandersDeck = new Deck<Bystander>();
+  mastermind: Mastermind;
+  scheme: Scheme;
+  villianDeck = new Deck<CardInVillainDeck>();
+
+  escapedVillian = new Deck<Villain>();
 
   constructor() {
-    this.shieldDeck.create(30, new hero_shield_officer);
-    this.woundsDeck.create(30, new wound);
-    this.bystandersDeck.create(30, new bystander);
+    /* SET UP */
+    /*********/
+    /* 1. player deck*/
     this.playerDeck.create(8, new hero_shield_agent);
     this.playerDeck.create(4, new hero_shield_trooper);
     this.playerDeck.shuffle();
+    /* 2. shield, bystanders, wounds deck */
+    this.shieldDeck.create(30, new hero_shield_officer);
+    this.bystandersDeck.create(30, new bystander);
+    this.woundsDeck.create(30, new wound);
+    /* 3. select mastermind */
+    this.mastermind = new mastermind_magneto;
+    /* 4. select scheme */
+    this.scheme = new scheme_legacy_virus;
+    /* 5. villain deck*/
+      /*  number of schemeTwist described in scheme */
+      /* a. 3 villain group 8*3=24 cards*/
+      /* b. 1 henchmen group 10 cards*/
+      /* c. 10 bystanders */
+      /* d. 5 masterStrike */
+      /* shuffle deck */
   }
 
   getKOimage(): Observable<string> { return this.koImage.asObservable(); }
