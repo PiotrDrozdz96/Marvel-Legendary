e1d6137efcbe8c3ef90eb3eef17d642f9c3588ee
Piotr Drożdż <piotr.drozdz0@op.pl>
2018-07-12 10:27:38 +0200

    add rest masterminds

--- src/app/cards/mastermind/mastermind.ts
+++ src/app/cards/mastermind/mastermind.ts
@@ -1,12 +1,60 @@
 import { Mastermind } from '../../models/card';
 
 // tslint:disable-next-line:class-name
+export class mastermind_doctor_doom implements Mastermind {
+    type = 'mastermind';
+    image = '/assets/cards/mastermind/doctor_doom/mastermind_doctor_doom.png';
+    attack = 9;
+    points = 5;
+    alwaysLeads = 'doombotLegion';
+    tactics = [
+        function() {},
+        function() {},
+        function() {},
+        function() {}
+    ];
+    masterStrike() { }
+}
+
+// tslint:disable-next-line:class-name
+export class mastermind_loki implements Mastermind {
+    type = 'mastermind';
+    image = '/assets/cards/mastermind/loki/mastermind_loki.png';
+    attack = 10;
+    points = 5;
+    alwaysLeads = 'enemiesOfAsgard';
+    tactics = [
+        function() {},
+        function() {},
+        function() {},
+        function() {}
+    ];
+    masterStrike() { }
+}
+
+// tslint:disable-next-line:class-name
 export class mastermind_magneto implements Mastermind {
     type = 'mastermind';
-    image = '/assets/cards/mastermind/magneto_mastermind.png';
+    image = '/assets/cards/mastermind/magneto/mastermind_magneto.png';
     attack = 8;
     points = 5;
     alwaysLeads = 'brotherhood';
+    tactics = [
+        function() {},
+        function() {},
+        function() {},
+        function() {}
+    ];
+    masterStrike() { }
+}
+
+// tslint:disable-next-line:class-name
+export class mastermind_red_skull implements Mastermind {
+    type = 'mastermind';
+    image = '/assets/cards/mastermind/red_skull/mastermind_red_skull.png';
+    attack = 7;
+    points = 5;
+    alwaysLeads = 'hydra';
     tactics = [
         function() {},
         function() {},
